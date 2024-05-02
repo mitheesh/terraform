6e1d2fa7f688321c51af9b0b7046aac6e9a897ac
mitheesh <mitheesh@gmail.com>
2024-05-02 05:29:38 +0000

    added client id

--- a1.provider.tf
+++ a1.provider.tf
@@ -1,5 +1,25 @@
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
 }
 
 provider "azurerm" {
@@ -10,10 +30,10 @@ provider "azurerm" {
 
     }
     backend "azurerm" {
-        resource_group_name = "terraform-storage"
-        storage_account_name = "terraformtfstaged1"
-        container = "tsstate"
-        key = "project.tfstate"
+      resource_group_name  = "terraform-storage"
+      storage_account_name = "terraformtfstaged1"
+      container            = "tsstate"
+      key                  = "project.tfstate"
     }
   }
   alias = "provider2-westus"
